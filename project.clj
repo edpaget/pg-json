6d89e5f714b03ef4653f6fa981f33b65abd5162b
Edward Paget <ed.paget@gmail.com>
2014-02-27 17:14:58 -0600

    Charge target and source jvm version debugging issue in another project"

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :javac-opts ["-target" "1.6" "-source" "1.6"]
   :dependencies [[org.postgresql/postgresql "9.3-1101-jdbc41"]]
   :profiles {:dev
              {:dependencies [[org.clojure/clojure "1.5.1"]]}})
