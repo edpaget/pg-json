c1a6aa4bf3feafeb69607e1a6204ccfa617bf64a
Edward Paget <ed.paget@gmail.com>
2014-03-19 10:28:40 -0500

    Remove json-storable protocol

--- src/pg_json/core.clj
+++ src/pg_json/core.clj
@@ -2,25 +2,14 @@
   (:require [cheshire.core :refer [generate-string parse-string]])
   (:import [org.postgresql.util PGobject]))
 
-(defprotocol JSONstorable
-  (to-json-column [this]))
+(defn to-json-column
+  [x]
+  (doto (PGobject.)
+    (.setType "json")
+    (.setValue (generate-string this))))
 
 (defprotocol JSONretrievable
   (from-json-column [this]))
-
-(extend-type clojure.lang.IPersistentMap
-  JSONstorable
-  (to-json-column [this]
-    (doto (PGobject.)
-      (.setType "json")
-      (.setValue (generate-string this)))))
-
-(extend-type clojure.lang.IPersistentVector
-  JSONstorable
-  (to-json-column [this]
-    (doto (PGobject.)
-      (.setType "json")
-      (.setValue (generate-string this)))))
 
 (extend-type org.postgresql.util.PGobject
   JSONretrievable
